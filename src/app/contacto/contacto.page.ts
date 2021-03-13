41f02ccb0b581bc9dabf07b3179e9ee21d6245cf
yancarloscuaran <yancarloscuaranimbacuan@gmail.com>
2021-03-13 10:48:26 -0500

    cargando datos

--- src/app/contacto/contacto.page.ts
+++ src/app/contacto/contacto.page.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.page.html',
   styleUrls: ['./contacto.page.scss'],
 })
-export class ContactoPage implements OnInit {
+export class ContactoPage{
 
-  constructor() { }
+  nombres = null;
 
-  ngOnInit() {
+  usuario = [];
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {}
+
+ 
+
+  async guardarDato(){
+    this.usuario.push(
+      {
+        nombres: this.nombres
+      }
+    ) 
+
+    console.log(this.usuario)
+    this.nombres = "";
+
+    
   }
 
+
 }
+
+
+ 
