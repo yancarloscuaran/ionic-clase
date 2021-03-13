6a61d6c132d0fe2a35c52e56080aab2192b46cd4
yancarloscuaran <yancarloscuaranimbacuan@gmail.com>
2021-03-13 12:29:25 -0500

    agregando saludo y operaciones

--- src/app/contacto/contacto.page.ts
+++ src/app/contacto/contacto.page.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AlertController, NavController } from '@ionic/angular';
-
-
+import { NavController, AlertController } from '@ionic/angular'
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.page.html',
@@ -9,29 +7,50 @@ import { AlertController, NavController } from '@ionic/angular';
 })
 export class ContactoPage{
 
+  constructor(public alertController: AlertController) { }
+
   nombres = null;
 
-  usuario = [];
-
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {}
-
- 
-
-  async guardarDato(){
-    this.usuario.push(
-      {
-        nombres: this.nombres
-      }
-    ) 
-
-    console.log(this.usuario)
-    this.nombres = "";
-
+  async saludar(){
+    if (this.nombres == null) {
+      const alerta = await this.alertController.create({
+        cssClass: 'my-custom-class',
+        header: 'Error',
+        message: "Ingresar Nombre ",
+        buttons: [
+          {
+            text: '',
+            role: 'cancel',
+            cssClass: 'secondary',
+            handler: () => {
+              console.log('OK');
+            }
+          }        
+        ]
+      });
+      this.nombres="",
+  
+      await alerta.present();
+    } else {
+      const alert = await this.alertController.create({
+        cssClass: 'my-custom-class',
+        header: 'Saludos',
+        message: "Hola " + this.nombres,
+        buttons: [
+          {
+            text: 'Cancelar',
+            role: 'cancel',
+            cssClass: 'secondary',
+            handler: () => {
+              console.log('Confirm Cancel');
+            }
+          }        
+        ]
+      });
+      this.nombres="",
+  
+      await alert.present();
+    }
     
   }
-
-
 }
-
-
- 
--- src/app/nosotros/nosotros.page.ts
+++ src/app/nosotros/nosotros.page.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, AlertController } from '@ionic/angular'
 
 @Component({
   selector: 'app-nosotros',
   templateUrl: './nosotros.page.html',
   styleUrls: ['./nosotros.page.scss'],
 })
-export class NosotrosPage implements OnInit {
+export class NosotrosPage {
+  num1 = null;
+  num2 = null;
+  operador = null;
+  respuesta = null;
+  operar = null;
+  
+  calcularDato(){
+    let number1 = parseInt(this.num1)
+    let number2 = parseInt(this.num2)
+    
+    switch(this.operador)
+    {
+      case '+':
+        this.operar = 'suma'
+        this.respuesta = number1 + number2
+      break;
 
-  constructor() { }
+      case '-':
+        this.operar = 'resta'
+        this.respuesta = number1 - number2
+      break;
 
-  ngOnInit() {
-  }
+      case '*':
+        this.operar = 'multiplicación'
+        this.respuesta = number1 * number2
+      break;
 
+      case '/':
+        this.operar = 'división'
+        this.respuesta = number1 / number2
+      break;
+    }
+  } 
 }
